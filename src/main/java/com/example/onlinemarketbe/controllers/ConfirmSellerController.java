49afd57630fc57e366fd5bd448730b2dcb1a2b36
ThuyLinh110 <thuylinh1102001@gmail.com>
2022-11-28 14:09:50 +0700

    update db Identity

---
+++ src/main/java/com/example/onlinemarketbe/controllers/ConfirmSellerController.java
@@ -0,0 +1,15 @@
+package com.example.onlinemarketbe.controllers;
+
+import com.example.onlinemarketbe.payload.request.SignupSellerRequest;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin()
+@RestController
+@RequestMapping("/api")
+@Api( tags = "manage-seller")
+public class ConfirmSellerController {
+
+
+
+}
