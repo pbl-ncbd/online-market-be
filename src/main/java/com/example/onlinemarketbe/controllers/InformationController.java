0df77ab2d069d922617fb2eb64d12c24bcc96846
ThuyLinh110 <thuylinh1102001@gmail.com>
2022-12-04 13:44:57 +0700

    update profile Ãof Ãuser

--- src/main/java/com/example/onlinemarketbe/controllers/InformationController.java
+++ src/main/java/com/example/onlinemarketbe/controllers/InformationController.java
@@ -1,13 +1,27 @@
 package com.example.onlinemarketbe.controllers;
 
+import com.example.onlinemarketbe.payload.request.InformationRequest;
+import com.example.onlinemarketbe.services.InformationService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @CrossOrigin()
 @RestController
 @RequestMapping("/api")
 @Tag( name = "profile")
 public class InformationController {
+
+    @Autowired
+    InformationService informationService;
+
+    @PostMapping("/update-profile")
+    @Operation(summary = "04/12/2022 by Linh : This is update profile of user by id")
+    public ResponseEntity<?> updateProfile(Principal principal , @RequestBody InformationRequest request){
+        return ResponseEntity.ok(informationService.updateProfile(principal.getName(), request));
+    }
 }
--- src/main/java/com/example/onlinemarketbe/repositories/InformationRepository.java
+++ src/main/java/com/example/onlinemarketbe/repositories/InformationRepository.java
@@ -1,7 +1,12 @@
 package com.example.onlinemarketbe.repositories;
 
 import com.example.onlinemarketbe.model.Information;
+import com.example.onlinemarketbe.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InformationRepository extends JpaRepository<Information, Integer> {
+
+    public Information findInformationById(int id);
+
+
 }
