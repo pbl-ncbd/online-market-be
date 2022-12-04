ca3816db8aa8dfef9b93cfb888a6e3c4be452930
ThuyLinh110 <thuylinh1102001@gmail.com>
2022-12-04 13:51:31 +0700

    update profile user

--- src/main/java/com/example/onlinemarketbe/model/Province.java
+++ src/main/java/com/example/onlinemarketbe/model/Province.java
@@ -1,39 +1,21 @@
 package com.example.onlinemarketbe.model;
+
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.sql.Date;
 
 @Getter
 @Setter
 @Entity
-public class Information {
-
+public class Province {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column
     @NotNull
     @Size(max = 255)
-    private String name;
-    @Column
-    @NotNull
-    @Size(max = 255)
-    private String phone;
-    @Column
-    @NotNull
-    private Date birthDate;
-
-    @ManyToOne
-    @JoinColumn(name = "district_id")
-    private District district;
-
-    @Column
-    @NotNull
-    private String address;
-
-
+    private String province_name;
 }
---
+++ src/main/java/com/example/onlinemarketbe/payload/request/InformationRequest.java
@@ -0,0 +1,77 @@
+package com.example.onlinemarketbe.payload.request;
+
+import com.example.onlinemarketbe.model.District;
+import com.example.onlinemarketbe.model.Province;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import javax.validation.constraints.NotNull;
+import java.sql.Date;
+
+public class InformationRequest {
+
+    @NotNull(message = "This is name of user")
+    @Schema(title = "name", example = "Nguyen Van A")
+    private String name;
+
+    @NotNull(message = "This is phone number of user")
+    @Schema(title = "phone", example = "0365999855")
+    private String phone;
+
+    @NotNull(message = "This is birth date of user")
+    @Schema(title = "birthDate")
+    private Date birthDate;
+
+    @NotNull(message = "This is address of user")
+    @Schema(title = "phone", example = "0365999855")
+    private String address;
+
+    @NotNull
+    @Schema(title = "district")
+    private int district_id;
+
+    public int getDistrict_id() {
+        return district_id;
+    }
+
+    public void setDistrict_id(int district_id) {
+        this.district_id = district_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    @NotNull
+    @Schema(title = "district")
+    private District district;
+
+}
